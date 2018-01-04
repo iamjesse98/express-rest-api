e4704e53910b889cff9f05d3f24ba8ca2185fac8
iamjesse98 <yadagallajaswanthkumar98@gmail.com>
2018-01-04 13:28:27 +0530

    error handling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 
@@ -9,6 +10,25 @@ const productRoutes = require('./api/routes/products');
 //     });
 // });
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
+
+// Error Handling
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    // res.status = error.status || 500;
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+    res.status(error.status || 500);
+});
 
 module.exports = app;
